da557291623a2960288a3682ccd66c549c9a0291
the-1aw <bguilliod@gmail.com>
2023-03-09 15:16:34 +0100

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import { createYoga } from "graphql-yoga";
+import { createServer } from "node:http";
+import { schema } from "./schema";
+
+const port = Number(process.env.API_PORT) || 4000;
+
+const yoga = createYoga({
+  schema: schema,
+});
+
+const server = createServer(yoga);
+
+server.listen(port, () => {
+  console.log(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`);
+});
